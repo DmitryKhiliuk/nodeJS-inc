930716e17bc7d4086cdef5f0a77a40def6208d72
DmitryKhiliuk <khiliukbrest@gmail.com>
2023-05-09 22:20:44 +0300

    add get endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import express, {Request, Response} from 'express'
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
+
+const products = [{title: 'tomato'}, {title: 'orange'}]
+const address = [{value: 'brest'}, {value: 'minsk'}]
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Test')
+})
+
+app.get('/products', (req: Request, res: Response) => {
+    res.send(products)
+})
+
+app.get('/address', (req: Request, res: Response) => {
+    res.send(address)
 })
 
 app.listen(port, () => {
