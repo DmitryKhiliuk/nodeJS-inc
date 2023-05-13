a12da63f1f130fd15efd3f39d631804885de5416
DmitryKhiliuk <khiliukbrest@gmail.com>
2023-05-13 12:24:43 +0300

    add get endpoints

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 const products = [{title: 'tomato'}, {title: 'orange'}]
-const address = [{value: 'brest'}, {value: 'minsk'}]
+const addresses = [{id: 1, value: 'brest' }, {id: 2, value: 'minsk' }]
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Test')
@@ -13,8 +13,26 @@ app.get('/products', (req: Request, res: Response) => {
     res.send(products)
 })
 
+app.get('/products/:productTitle', (req: Request, res: Response) => {
+    let product = products.find(p => p.title === req.params.productTitle);
+    if (product) {
+        res.send(product)
+    } else {
+        res.send(404)
+    }
+})
+
 app.get('/address', (req: Request, res: Response) => {
-    res.send(address)
+    res.send(addresses)
+})
+
+app.get('/address/:id', (req: Request, res: Response) => {
+    let address = addresses.find(p => p.id === +req.params.id);
+    if (address) {
+        res.send(address)
+    } else {
+        res.send(404)
+    }
 })
 
 app.listen(port, () => {
